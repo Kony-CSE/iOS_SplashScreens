2c21e15a75f1254c05d033ba6f09816134b09e2f
Sirisha <sirisha.munala@kony.com>
2016-09-01 01:05:50 +0530

    Commit on iOS Splash screens

---
+++ iOS_Splashscreens/jssrc/iphone/generated/Test.js
@@ -0,0 +1,48 @@
+//Form JS File
+function addWidgetsTest() {};
+
+function TestGlobals() {
+    var MenuId = [];
+    Test = new kony.ui.Form2({
+        "id": "Test",
+        "enableScrolling": true,
+        "bounces": true,
+        "allowHorizontalBounce": true,
+        "allowVerticalBounce": true,
+        "pagingEnabled": false,
+        "needAppMenu": true,
+        "title": null,
+        "enabledForIdleTimeout": false,
+        "skin": "Cyan",
+        "bouncesZoom": true,
+        "zoomScale": 1.0,
+        "minZoomScale": 1.0,
+        "maxZoomScale": 1.0,
+        "layoutType": kony.flex.FREE_FORM,
+        "addWidgets": addWidgetsTest
+    }, {
+        "padding": [0, 0, 0, 0],
+        "displayOrientation": constants.FORM_DISPLAY_ORIENTATION_PORTRAIT,
+        "paddingInPixel": false
+    }, {
+        "retainScrollPosition": false,
+        "needsIndicatorDuringPostShow": true,
+        "formTransparencyDuringPostShow": "100",
+        "inputAccessoryViewType": constants.FORM_INPUTACCESSORYVIEW_DEFAULT,
+        "configureExtendTop": false,
+        "configureExtendBottom": false,
+        "configureStatusBarStyle": false,
+        "titleBar": true,
+        "footerOverlap": false,
+        "headerOverlap": false,
+        "inTransitionConfig": {
+            "transitionDirection": "none",
+            "transitionEffect": "none"
+        },
+        "outTransitionConfig": {
+            "transitionDirection": "none",
+            "transitionEffect": "none"
+        }
+    });
+    Test.setDefaultUnit(kony.flex.DP);
+};
---
+++ iOS_Splashscreens/jssrc/iphone/startup/startup.js
@@ -0,0 +1,63 @@
+//startup.js file
+var globalhttpheaders = {};
+var appConfig = {
+    appId: "LaunchScreen",
+    appName: "LaunchScreen",
+    appVersion: "1.0.0",
+    platformVersion: null,
+    serverIp: "10.10.3.30",
+    serverPort: "80",
+    secureServerPort: "443",
+    isDebug: true,
+    middlewareContext: "middleware",
+    isMFApp: false,
+    eventTypes: [],
+    secureurl: "https://10.10.3.30:443/middleware/MWServlet",
+    url: "http://10.10.3.30:80/middleware/MWServlet"
+};
+sessionID = "";
+
+function appInit(params) {
+    skinsInit();
+    kony.application.setCheckBoxSelectionImageAlignment(constants.CHECKBOX_SELECTION_IMAGE_ALIGNMENT_RIGHT);
+    kony.application.setDefaultTextboxPadding(false);
+    kony.application.setRespectImageSizeForImageWidgetAlignment(true);
+    TestGlobals();
+    setAppBehaviors();
+};
+
+function setAppBehaviors() {
+    kony.application.setApplicationBehaviors({
+        applyMarginPaddingInBCGMode: false,
+        adherePercentageStrictly: true,
+        retainSpaceOnHide: true,
+        marginsIncludedInWidgetContainerWeight: true,
+        APILevel: 6500
+    })
+};
+
+function themeCallBack() {
+    kony.application.setApplicationInitializationEvents({
+        init: appInit,
+        showstartupform: function() {
+            Test.show();
+        }
+    });
+};
+
+function loadResources() {
+    globalhttpheaders = {};
+    sdkInitConfig = {
+        "appConfig": appConfig,
+        "isMFApp": appConfig.isMFApp,
+        "eventTypes": appConfig.eventTypes
+    }
+    kony.setupsdks(sdkInitConfig, null, null);
+    kony.theme.setCurrentTheme("default", themeCallBack, themeCallBack);
+};
+kony.application.setApplicationMode(constants.APPLICATION_MODE_NATIVE);
+//If default locale is specified. This is set even before any other app life cycle event is called.
+loadResources();
+// If you wish to debug Application Initialization events, now is the time to
+// place breakpoints.
+debugger;
